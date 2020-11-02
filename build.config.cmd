771539e9335e69ee1a7502cd344569b54b67c396
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:31:56 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-openjdk
 set PRODUCT_VERSION=15.0.1
+set PRODUCT_BASE=open
--- build.installer.cmd
+++ build.installer.cmd
@@ -3,9 +3,9 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> installer openjdk
-
 call build.config.cmd
+
+echo -^> installer %PRODUCT_NAME%
 
 if exist installer\ rmdir /Q /S installer
 mkdir installer
@@ -16,7 +16,7 @@ mkdir build
 makensis.exe /NOCD "util\openjdk-installer.nsi"
 makensis.exe /NOCD "util\openjre-installer.nsi"
 
-call grigore-stefan.sign "OpenJDK" "installer\openjdk-%PRODUCT_VERSION%-installer.exe"
-call grigore-stefan.sign "OpenJRE" "installer\openjre-%PRODUCT_VERSION%-installer.exe"
+call grigore-stefan.sign "OpenJDK" "installer\%PRODUCT_BASE%jdk-%PRODUCT_VERSION%-installer.exe"
+call grigore-stefan.sign "OpenJRE" "installer\%PRODUCT_BASE%jre-%PRODUCT_VERSION%-installer.exe"
 
 if exist build\ rmdir /Q /S build
