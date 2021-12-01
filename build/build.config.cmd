a26b845a05687abfda44d93c26cdbb5e1f48e91c
Grigore Stefan <g_stefan@yahoo.com>
2021-12-01 03:02:16 +0200

    Update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-openjdk
-set PRODUCT_VERSION=16
+set PRODUCT_VERSION=17.0.1
 set PRODUCT_BASE=open
--- build/build.vendor.cmd
+++ build/build.vendor.cmd
@@ -9,5 +9,5 @@ echo -^> vendor %PRODUCT_NAME%
 
 if not exist vendor\ mkdir vendor
 
-set WEB_LINK=https://download.java.net/java/GA/jdk%PRODUCT_VERSION%/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-%PRODUCT_VERSION%_windows-x64_bin.zip
+set WEB_LINK=https://download.java.net/java/GA/jdk17.0.1/2a2082e5a09d4267845be086888add4f/12/GPL/openjdk-17.0.1_windows-x64_bin.zip
 if not exist vendor\openjdk-%PRODUCT_VERSION%_windows-x64_bin.zip curl --insecure --location %WEB_LINK% --output vendor\openjdk-%PRODUCT_VERSION%_windows-x64_bin.zip
