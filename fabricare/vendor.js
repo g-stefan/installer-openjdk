fc8948b9f38311f771445d5a624373bbb4632506
Grigore Stefan <g_stefan@yahoo.com>
2024-10-14 16:57:14 +0300

    Update

--- fabricare/vendor.js
+++ fabricare/vendor.js
@@ -6,10 +6,10 @@
 messageAction("vendor");
 
 Shell.mkdirRecursivelyIfNotExists("vendor");
-
+		
 var vendor = "openjdk-" + Project.version + "_windows-x64_bin.zip";
 if (!Shell.fileExists("vendor/" + vendor)) {
-	var webLink = "https://download.java.net/java/GA/jdk" + Project.version + "/afdd2e245b014143b62ccb916125e3ce/10/GPL/" + vendor;
+	var webLink = "https://download.java.net/java/GA/jdk" + Project.version + "/3c5b90190c68498b986a97f276efd28a/37/GPL/" + vendor;
 	var cmd = "curl --insecure --location " + webLink + " --output vendor/" + vendor;
 	Console.writeLn(cmd);
 	exitIf(Shell.system(cmd));
